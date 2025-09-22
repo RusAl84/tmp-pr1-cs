117faa0c8d8e4def0a43dad8a0ee635c4b5a932e
Huawei <Huawei@LAPTOP-FKEMLJA8>
2025-09-22 13:40:44 +0300

    fix

--- tmp-pr1-cs/Program.cs
+++ tmp-pr1-cs/Program.cs
@@ -1,4 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int n = 0;
-int s=
+﻿long nop = 0;  // number of operations
+for(int n = 2; n <= 16; n++) { 
+    nop++;
+    int s = 0; nop++;
+    nop+=4;
+    for (int i = n; i < 2*n*n; i++)
+    {
+        nop += 5;
+        nop += 5;
+        for (int j = 2*n; j < n * n+1; j++)
+        {
+            nop += 5; 
+            //Console.WriteLine("Александр не съел {0} ЁЖИКОВ", i);
+            //Console.WriteLine("ЁЖИК не съел {0} Сашу", j);
+            int k = 13; nop++;
+            nop+=2;
+            while (k +3 <42) {
+                nop += 2;
+                k += 7; nop += 2;
+                s++; nop += 2;
+            }
+        }
+    }
+    nop++;
+    //Console.WriteLine("s = {0} nop = {1} ", s, nop); 
+    Console.WriteLine("{0}, {1} ", n, nop); 
+}
+
